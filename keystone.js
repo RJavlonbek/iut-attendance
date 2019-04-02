55019fa4f8f7524f6853f530a16f5ceb6a30d3a6
RJavlonbek <33687903+RJavlonbek@users.noreply.github.com>
2019-04-02 18:14:23 +0500

    Update keystone.js

--- keystone.js
+++ keystone.js
@@ -43,7 +43,7 @@ keystone.import('models');
 // for each request) should be added to ./routes/middleware.js
 keystone.set('locals', {
 	_: require('lodash'),
-	env: keystone.get('env'),
+	env: 'development',
 	utils: keystone.utils,
 	editable: keystone.content.editable,
 });

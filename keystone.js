9d60d0f4fc562781d9e33789b8e87d804b839281
RJavlonbek <33687903+RJavlonbek@users.noreply.github.com>
2019-04-03 08:58:26 +0500

    Update keystone.js

--- keystone.js
+++ keystone.js
@@ -55,3 +55,28 @@ keystone.import('models');
 // Setup common locals for your templates. The following are required for the
 // bundled templates and layouts. Any runtime locals (that should be set uniquely
 // 
+
+keystone.set('locals', {
+	_: require('lodash'),
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	editable: keystone.content.editable,
+});
+
+// Load your project's Routes
+keystone.set('routes', require('./routes'));
+
+
+// Configure the navigation bar in Keystone's Admin UI
+keystone.set('nav', {
+	enquiries: 'enquiries',
+	users: 'users',
+});
+
+// Start Keystone to connect to your database and initialise the web server
+
+//require('./store-faces');
+
+
+
+keystone.start();
